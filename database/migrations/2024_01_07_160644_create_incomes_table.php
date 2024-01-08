0c3bb574b2d70be1d47dfd76338e37ed06d6bf09
ttnppedr <priest0204@gmail.com>
2024-01-08 09:52:25 +0800

    Add Income model

---
+++ database/migrations/2024_01_07_160644_create_incomes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('incomes', function (Blueprint $table) {
+            $table->id();
+            $table->string('member_name');
+            $table->string('title');
+            $table->unsignedInteger('amount');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('incomes');
+    }
+};
